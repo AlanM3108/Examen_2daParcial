c1e308feb892dc4675d6d5c5516b80ed8c35e1af
AlanM3108 <118567896+AlanM3108@users.noreply.github.com>
2024-04-15 11:52:57 -0600

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TAREA.Data
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "El # de inventario es Reuerido")]
+        [MaxLength(100, ErrorMessage = "Debe ser un valor positivo y unico Valido")]
+        public string? Inventario { get; set; }
+        
+        [Required(ErrorMessage = "Requiere nombre de Producto")]
+        [MaxLength(10, ErrorMessage = "Maximo 10 letras")]
+        public string? Nombre { get; set; }
+
+        
+        public string? Departamento { get; set; }
+    // [Required(ErrorMessage = "Debe seleccionar el género")] public string Genero { get; set; }
+
+}
+}
